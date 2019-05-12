f02de3e6258d9e5091016a0cb6c7197b21365d8c
YAHOO\yasuaki <yasuakihappy@gmail.com>
2019-05-12 10:44:40 +0900

    first commit

---
+++ resources/views/top_list.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="ja">
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>codelike bbs</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">codelike bbs</a>
+    </nav>
+    <div class="container">
+      <div class="mt-3 mb-3 row justify-content-end">
+          <a href="" class="btn btn-primary mr-3">新規登録</a>
+      </div>
+      <ul class="list-group">
+        <li class="list-group-item">
+          <a href="">とりあえずなリスト１</a>
+        </li>
+        <li class="list-group-item">
+          <a href="">とりあえずなリスト２</a>
+        </li>
+        <li class="list-group-item">
+          <a href="">とりあえずなリスト３</a>
+        </li>
+      </ul>
+    </div>
+  </body>
+</html>
