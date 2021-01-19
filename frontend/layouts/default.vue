9006e2306d0c6012d5d0d8da03f183a14b299747
yasuaki <yasuakihappy2@yahoo.co.jp>
2021-01-19 16:04:34 +0900

    update default layout view

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,20 +1,8 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+    <v-navigation-drawer v-model="drawer" fixed app>
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact v-show="item.isShow">
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -24,108 +12,98 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+    <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
     <v-snackbar v-model="warningBar" color="warning">{{warningMessage}}</v-snackbar>
   </v-app>
 </template>
 
-<script>
-export default {
-  mounted() {
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator';
+import { isUserSignIn } from '~/utils';
+
+@Component({
+  name: 'LayoutDefaultPage',
+})
+export default class LayoutDefaultPage extends Vue {
+  @Prop()
+  clipped:boolean = false;
+
+  @Prop()
+  drawer:boolean = false;
+
+  @Prop()
+  fixed:boolean = false;
+
+  @Prop()
+  items:Array<any> = [
+    {
+      icon: 'mdi-apps',
+      title: 'Top',
+      to: '/',
+      isShow: true
+    },
+    {
+      icon: 'mdi-chart-bubble',
+      title: 'SignIn',
+      to: '/login',
+      isShow: false
+    }
+  ];
+
+  @Prop()
+  miniVariant:boolean = false;
+
+  @Prop()
+  right:boolean = true;
+
+  @Prop()
+  rightDrawer:boolean = false;
+
+  @Prop()
+  title:string = 'codelike bbs';
+
+  @Prop()
+  warningBar:boolean = false;
+
+  @Prop()
+  warningMessage:string = '';
+
+  @Prop()
+  signedInMenuIndex: number = 1;
+
+  private async mounted() {
+    const signedIn = await isUserSignIn();
+    if (!signedIn) {
+      this.items[this.signedInMenuIndex].isShow = true;
+    }
+
     this.$nuxt.$on('warningSnackbar', this.onWarningSnackbar);
-  },
-  beforeDestroy() {
+    this.$nuxt.$on('signedIn', this.onSignedIn);
+  }
+
+  private beforeDestroy() {
     this.$nuxt.$off('warningSnackbar')
-  },
-  methods:{
-    onWarningSnackbar(message) {
+    this.$nuxt.$off('signedIn')
+  }
+
+  private onWarningSnackbar(message: string) {
       this.warningBar = true;
       this.warningMessage = message;
-    }
-  },
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Top',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'SignIn',
-          to: '/login'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'codelike bbs',
-      warningBar: false,
-    }
+  }
+
+  private onSignedIn() {
+    this.items[this.signedInMenuIndex].isShow = false;
   }
 }
 </script>
